8b07f1e02a98a699ac7a1ac1ec02cd0f030a7060
[dyad] <tonygomes058@gmail.com>
2025-11-07 14:50:12 -0300

    [dyad] Correção do logo quebrado - wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,9 @@
 import { cn } from "@/lib/utils";
+import logoUrl from "/logo-mac-ip.png";
 
 export const Logo = ({ className }: { className?: string }) => (
   <img
-    src="/logo-mac-ip.png"
+    src={logoUrl}
     alt="MAC-IP Logo"
     className={cn("h-auto", className)}
   />
